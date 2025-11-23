f8175bd1fe910ee1687118a93243e3d094106174
Pavel Bibikov <mesoulte@gmail.com>
2025-11-23 20:12:07 +0300

    fix: fixed config for for docker operation

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,11 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite' 
+import { defineConfig } from 'vite' // Вернули импорт из 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // @ts-ignore
   test: {
     environment: 'jsdom',
     globals: true,
